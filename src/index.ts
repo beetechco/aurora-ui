3aeab9fb697316db06f5ef7e0685cccbd1de7638
samuel@mi.com.co <samuel@samueljimenez.co>
2019-01-08 13:04:06 -0500

    Aurora Theme Provider

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { Button, UIRegistry } from 'aurora';
+import { ThemeContext } from './ui/theme/ThemeContext';
 import { ButtonView } from './ui/button/ButtonView';
 
-export const initAuroraAntd = () => {
+export const initAuroraUI = () => {
   UIRegistry.register(
     Button.UICODE as string,
     (model: any, id: string) => {
       return new ButtonView(model, id);
     });
 }
+
+export {
+  ThemeContext
+};
